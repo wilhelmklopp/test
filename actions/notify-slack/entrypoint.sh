83e2d36acf05fc31bee4ad307a09ee8fb87cacc6
Wilhelm Klopp <wil.klopp@gmail.com>
2019-01-12 12:48:52 +0000

    Add files

---
+++ actions/notify-slack/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -eu
+
+echo $GITHUB_TOKEN
+echo $GITHUB_REPOSITORY
+
+curl -H "Authorization: token $GITHUB_TOKEN" \
+    -XPOST "https://slack.github.com/repos/$GITHUB_REPOSITORY" \
+    -d '{"text": "Hello GitHub+Slack API"}'
